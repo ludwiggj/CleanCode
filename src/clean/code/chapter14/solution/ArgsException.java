0aaa14002f77e2eb13dcf714b4d3712a163fa7c5
ludwiggj <graeme.ludwig@btopenworld.com>
2014-06-27 12:15:52 +0100

    [Graeme] Fixed parsing of boolean arguments in official Args solution

--- src/clean/code/chapter14/solution/ArgsException.java
+++ src/clean/code/chapter14/solution/ArgsException.java
@@ -5,9 +5,12 @@ public class ArgsException extends Exception {
   private String errorParameter = null;
   private ErrorCode errorCode = ErrorCode.OK;
 
-  public ArgsException() {}
+  public ArgsException() {
+  }
 
-  public ArgsException(String message) {super(message);}
+  public ArgsException(String message) {
+    super(message);
+  }
 
   public ArgsException(ErrorCode errorCode) {
     this.errorCode = errorCode;
@@ -17,6 +20,7 @@ public class ArgsException extends Exception {
     this.errorCode = errorCode;
     this.errorParameter = errorParameter;
   }
+
   public ArgsException(ErrorCode errorCode,
                        char errorArgumentId, String errorParameter) {
     this.errorCode = errorCode;
@@ -56,32 +60,42 @@ public class ArgsException extends Exception {
         return String.format("Argument -%c unexpected.", errorArgumentId);
       case MISSING_STRING:
         return String.format("Could not find string parameter for -%c.",
-                             errorArgumentId);
+            errorArgumentId);
       case INVALID_INTEGER:
         return String.format("Argument -%c expects an integer but was '%s'.",
-                             errorArgumentId, errorParameter);
+            errorArgumentId, errorParameter);
       case MISSING_INTEGER:
         return String.format("Could not find integer parameter for -%c.",
-                             errorArgumentId);
+            errorArgumentId);
       case INVALID_DOUBLE:
         return String.format("Argument -%c expects a double but was '%s'.",
-                             errorArgumentId, errorParameter);
+            errorArgumentId, errorParameter);
       case MISSING_DOUBLE:
         return String.format("Could not find double parameter for -%c.",
-                             errorArgumentId);
+            errorArgumentId);
+      case MISSING_BOOLEAN:
+        return String.format("Could not find boolean parameter for -%c.",
+            errorArgumentId);
       case INVALID_ARGUMENT_NAME:
         return String.format("'%c' is not a valid argument name.",
-                             errorArgumentId);
+            errorArgumentId);
       case INVALID_ARGUMENT_FORMAT:
         return String.format("'%s' is not a valid argument format.",
-                             errorParameter);
+            errorParameter);
     }
     return "";
+  }
+
+  @Override
+  public String getMessage() {
+    return errorMessage();
   }
 
   public enum ErrorCode {
     OK, INVALID_ARGUMENT_FORMAT, UNEXPECTED_ARGUMENT, INVALID_ARGUMENT_NAME,
     MISSING_STRING,
     MISSING_INTEGER, INVALID_INTEGER,
-    MISSING_DOUBLE, INVALID_DOUBLE}
+    MISSING_DOUBLE, INVALID_DOUBLE,
+    MISSING_BOOLEAN
+  }
 }
--- src/clean/code/chapter14/solution/BooleanArgumentMarshaler.java
+++ src/clean/code/chapter14/solution/BooleanArgumentMarshaler.java
@@ -1,18 +1,27 @@
 package clean.code.chapter14.solution;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static clean.code.chapter14.solution.ArgsException.ErrorCode.MISSING_BOOLEAN;
 
 public class BooleanArgumentMarshaler implements ArgumentMarshaler {
   private boolean booleanValue = false;
 
-    public void set(Iterator<String> currentArgument) throws ArgsException {
-      booleanValue = true;
+  public void set(Iterator<String> currentArgument) throws ArgsException {
+    String parameter = null;
+    try {
+      parameter = currentArgument.next();
+      booleanValue = Boolean.parseBoolean(parameter);
+    } catch (NoSuchElementException e) {
+      throw new ArgsException(MISSING_BOOLEAN);
     }
+  }
 
-    public static boolean getValue(ArgumentMarshaler am) {
-      if (am != null && am instanceof BooleanArgumentMarshaler)
-        return ((BooleanArgumentMarshaler) am).booleanValue;
-      else
-        return false;
-    }
+  public static boolean getValue(ArgumentMarshaler am) {
+    if (am != null && am instanceof BooleanArgumentMarshaler)
+      return ((BooleanArgumentMarshaler) am).booleanValue;
+    else
+      return false;
+  }
 }
